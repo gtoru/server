27859a2fa8d4d922cb657964133024e1fee3921f
madetara <madetara@yandex.com>
2020-04-04 00:47:33 +0300

    feat(server): add authentication manager

---
+++ server/src/server.core/Application/AuthenticationManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using server.core.Domain;
+using server.core.Domain.Authentication;
+using server.core.Infrastructure;
+using server.core.Infrastructure.Error;
+
+namespace server.core.Application
+{
+    public static class AuthenticationManager
+    {
+        public static async Task<User> RegisterUserAsync(IUnitOfWork unitOfWork, string email, string password)
+        {
+            var user = User.CreateNew(email, password);
+
+            await unitOfWork.Users.AddUserAsync(user);
+
+            return user;
+        }
+
+        public static async Task<Session> AuthenticateAsync(IUnitOfWork unitOfWork, string email, string password)
+        {
+            var user = await unitOfWork.Users.FindUserAsync(email);
+
+            user.Password.Verify(password);
+
+            Session session;
+            try
+            {
+                session = await unitOfWork.Sessions.FindSessionByUserAsync(user.UserId);
+            }
+            catch (SessionNotFoundException)
+            {
+                session = Session.CreateNew(user.UserId);
+                await unitOfWork.Sessions.AddSessionAsync(session);
+            }
+
+            return session;
+        }
+
+        public static async Task CheckSessionAsync(IUnitOfWork unitOfWork, Guid sessionId)
+        {
+            var session = await unitOfWork.Sessions.FindSessionAsync(sessionId);
+
+            session.CheckSession();
+
+            session.Prolongate();
+        }
+    }
+}
