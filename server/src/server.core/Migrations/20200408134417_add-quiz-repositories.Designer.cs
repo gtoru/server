ab63dba71111ebbd848233786b0457ea19a0e32e
madetara <madetara@yandex.com>
2020-04-08 16:46:04 +0300

    feat(server): add migrations for quiz repositories

--- server/src/server.core/Migrations/20200408134417_add-quiz-repositories.Designer.cs
+++ server/src/server.core/Migrations/20200408134417_add-quiz-repositories.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using server.core.Infrastructure;
@@ -10,9 +11,10 @@ using server.core.Infrastructure;
 namespace server.core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200408134417_add-quiz-repositories")]
+    partial class addquizrepositories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
